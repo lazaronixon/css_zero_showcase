57829152088cc5618956ef2279bcba2d5e7b6e6a
Nixon <lazaronixon@hotmail.com>
2024-07-11 05:17:24 -0300

    badge

---
+++ test/components/previews/badge_preview.rb
@@ -0,0 +1,19 @@
+# @css app/assets/stylesheets/badge.css
+class BadgePreview < Lookbook::Preview
+  # @!group Basic
+  def primary
+  end
+
+  def secondary
+  end
+
+  def outline
+  end
+
+  def positive
+  end
+
+  def negative
+  end
+  # @!endgroup
+end
