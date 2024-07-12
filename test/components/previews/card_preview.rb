82c87233ea378aa9640cd395ffcbe61dc168d477
Nixon <lazaronixon@hotmail.com>
2024-07-11 21:02:43 -0300

    more work

--- test/components/previews/card_preview.rb
+++ test/components/previews/card_preview.rb
@@ -1,3 +1,4 @@
+# @display max_width 350px
 # @css app/assets/stylesheets/card.css
 class CardPreview < Lookbook::Preview
   def basic
---
+++ test/components/previews/dialog_preview.rb
@@ -0,0 +1,9 @@
+# @css app/assets/stylesheets/dialog.css
+# @js app/javascript/controllers/dialog_controller.js
+class DialogPreview < Lookbook::Preview
+  def alert_dialog
+  end
+
+  def dismissible_dialog
+  end
+end
