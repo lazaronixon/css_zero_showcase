15d2e676c88ec2354afa0326a5281773c00f379c
Nixon <lazaronixon@hotmail.com>
2024-07-13 03:52:08 -0300

    progress

---
+++ test/components/previews/progress_preview.rb
@@ -0,0 +1,11 @@
+# @display max_width 450px
+# @css app/assets/stylesheets/progress.css
+class ProgressPreview < Lookbook::Preview
+  # @!group Progress
+  def default
+  end
+
+  def indeterminate
+  end
+  # @!endgroup
+end
