394a69115d447be544f6d7af26ac42085241bed7
Nixon <lazaronixon@hotmail.com>
2024-07-14 01:40:31 -0300

    Separator

---
+++ test/components/previews/separator_preview.rb
@@ -0,0 +1,14 @@
+# @display max_width 450px
+# @css app/assets/stylesheets/progress.css
+class SeparatorPreview < Lookbook::Preview
+  # @!group Separator
+  def default
+  end
+
+  def inline_solid
+  end
+
+  def inline_dashed
+  end
+  # @!endgroup
+end
