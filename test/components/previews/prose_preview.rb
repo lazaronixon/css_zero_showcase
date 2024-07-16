35d30fd6bb475b9eccca1ac65c88e8974c7d753e
Nixon <lazaronixon@hotmail.com>
2024-07-15 22:17:04 -0300

    add prose component

---
+++ test/components/previews/prose_preview.rb
@@ -0,0 +1,25 @@
+# @css app/assets/stylesheets/prose.css
+class ProsePreview < Lookbook::Preview
+  def headings
+  end
+
+  def paragraphs
+  end
+
+  def lists
+  end
+
+  # @!group Code
+  def inline_code
+  end
+
+  def code_block
+  end
+  # @!endgroup
+
+  def table
+  end
+
+  def blockquote
+  end
+end
