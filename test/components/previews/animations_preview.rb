138cc8ce31dda741577e321aae484083d8b07d28
Nixon <lazaronixon@hotmail.com>
2024-07-16 19:18:01 -0300

    cosmetic

--- test/components/previews/animations_preview.rb
+++ test/components/previews/animations_preview.rb
@@ -1,5 +1,6 @@
 class AnimationsPreview < Lookbook::Preview
-  # These animations are available as CSS variables in animation.css.
+  # These animations are available as CSS variables in animation.css,
+  # the animation stimulus controller is for demonstration purposes only.
   def default
   end
 end
