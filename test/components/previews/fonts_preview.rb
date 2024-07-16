373e0017723171cf85442c13c1d8f40423620ad3
Nixon <lazaronixon@hotmail.com>
2024-07-16 14:24:48 -0300

    Fonts preview

---
+++ test/components/previews/fonts_preview.rb
@@ -0,0 +1,49 @@
+class FontsPreview < Lookbook::Preview
+  # @!group Default
+  # These fonts are available as CSS variables in typography.css.
+  def system_ui
+  end
+
+  def transational
+  end
+
+  def old_style
+  end
+
+  def humanist
+  end
+
+  def geometric_humanist
+  end
+
+  def classical_humanist
+  end
+
+  def neo_grotesque
+  end
+
+  def monospace_slab_serif
+  end
+
+  def monospace_code
+  end
+
+  def industrial
+  end
+
+  def rounded_sans
+  end
+
+  def slab_serif
+  end
+
+  def antique
+  end
+
+  def didone
+  end
+
+  def handwritten
+  end
+  # @!endgroup
+end
