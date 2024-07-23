59704647567faa7d168a8c70af1ca5c24a2195f0
Nixon <lazaronixon@hotmail.com>
2024-07-23 02:32:23 -0300

    remove notes

--- test/components/previews/aspect_ratio_preview.rb
+++ test/components/previews/aspect_ratio_preview.rb
@@ -1,6 +1,5 @@
 # @display max_width 635px
 class AspectRatioPreview < Lookbook::Preview
-  # No components are required to implement this feature.
   def default
   end
 end
--- test/components/previews/scroll_area_preview.rb
+++ test/components/previews/scroll_area_preview.rb
@@ -1,5 +1,4 @@
 class ScrollAreaPreview < Lookbook::Preview
-  # No components are required to implement this feature.
   def default
   end
 end
