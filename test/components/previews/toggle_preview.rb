8e33683aac68445936a35a2738fa806b0aea769c
Nixon <lazaronixon@hotmail.com>
2024-07-25 02:00:25 -0300

    Add toggle buttons

---
+++ test/components/previews/toggle_preview.rb
@@ -0,0 +1,16 @@
+# @css app/assets/stylesheets/toggle.css
+class TogglePreview < Lookbook::Preview
+  # @!group Toggle
+  def default
+  end
+
+  def outline
+  end
+
+  def with_text
+  end
+  # @!endgroup
+
+  def group
+  end
+end
