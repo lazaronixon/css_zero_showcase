0ad296ce2007d47b101b64d9307ccf2475b29db4
Nixon <lazaronixon@hotmail.com>
2024-08-29 02:06:00 -0300

    add tabs

---
+++ test/components/previews/tabs_preview.rb
@@ -0,0 +1,7 @@
+# @css app/assets/stylesheets/tabs.css
+# @js app/javascript/controllers/tabs_controller.js
+class TabsPreview < Lookbook::Preview
+  # https://github.com/lazaronixon/css-zero/blob/master/app/helpers/tabs_helper.rb
+  def default
+  end
+end
