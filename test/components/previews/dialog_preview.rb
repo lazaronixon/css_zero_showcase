e905176a58dbcbf719b94a01e5cc5f06416d4fc0
Nixon <lazaronixon@hotmail.com>
2024-09-10 02:52:05 -0300

    Remove button_to_close_dialog

--- test/components/previews/dialog_preview.rb
+++ test/components/previews/dialog_preview.rb
@@ -4,7 +4,6 @@ class DialogPreview < Lookbook::Preview
   def alert_dialog
   end
 
-  # Use `button_to_close_dialog` to render a button that will close the dialog.
   def dismissible_dialog
   end
 end
