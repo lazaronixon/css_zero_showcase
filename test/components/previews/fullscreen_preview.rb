0819443078d7beb29050cb6db655b39bf313f0a7
Nixon <lazaronixon@hotmail.com>
2024-09-17 04:53:11 -0300

    fullscreen button

---
+++ test/components/previews/fullscreen_preview.rb
@@ -0,0 +1,12 @@
+# @js app/javascript/controllers/fullscreen_controller.js
+class FullscreenPreview < Lookbook::Preview
+  # Example
+  # ---
+  # ```html
+  # <body data-controller="fullscreen">
+  #   <%= yield %>
+  # </body>
+  # ```
+  def default
+  end
+end
