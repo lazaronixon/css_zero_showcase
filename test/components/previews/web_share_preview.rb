6fafb80a792d761c0a6c8c233e295bc6dbc5b772
Nixon <lazaronixon@hotmail.com>
2024-09-18 12:34:58 -0300

    Add web share preview

---
+++ test/components/previews/web_share_preview.rb
@@ -0,0 +1,11 @@
+# @js app/javascript/controllers/web_share_controller.js
+class WebSharePreview < Lookbook::Preview
+  # Your browser must support web share.
+  # @!group Web Share
+  def url
+  end
+
+  def files
+  end
+  # @!endgroup
+end
