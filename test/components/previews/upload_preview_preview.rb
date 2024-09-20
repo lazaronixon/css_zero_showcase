56537809f2c390ee079d24ecdbc79f52f25764ef
Nixon <lazaronixon@hotmail.com>
2024-09-19 23:04:25 -0300

    upload preview

---
+++ test/components/previews/upload_preview_preview.rb
@@ -0,0 +1,13 @@
+# @js app/javascript/controllers/upload_preview_controller.js
+class UploadPreviewPreview < Lookbook::Preview
+  # Example
+  # ---
+  # You can remove the attachment in the same
+  # request using the hidden input value.
+  #
+  # ```ruby
+  # user.avatar.purge if params[:remove_avatar] == 'true'
+  # ```
+  def default
+  end
+end
