1c89d632ea436cc939a78b41dc35c13305aeb8fe
Nixon <lazaronixon@hotmail.com>
2024-09-23 01:49:02 -0300

    more work lightbox

--- app/javascript/controllers/lightbox_controller.js
+++ app/javascript/controllers/lightbox_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = [ "dialog", "zoomedImage" ]
+  static targets = [ "dialog", "zoomedImage", "download", "share" ]
 
   show(event) {
     this.dialogTarget.showModal()
@@ -14,9 +14,13 @@ export default class extends Controller {
 
   reset() {
     this.zoomedImageTarget.src = ""
+    this.downloadTarget.href = ""
+    this.shareTarget.dataset.webShareFileValue = "";
   }
 
   #set(target) {
     this.zoomedImageTarget.src = target.href
+    this.downloadTarget.href = target.dataset.lightboxUrlValue;
+    this.shareTarget.dataset.webShareFileValue = target.dataset.lightboxUrlValue;
   }
 }
--- test/components/previews/lightbox_preview.rb
+++ test/components/previews/lightbox_preview.rb
@@ -1,6 +1,13 @@
 # @css app/assets/stylesheets/lightbox.css
 # @js app/javascript/controllers/lightbox_controller.js
 class LightboxPreview < Lookbook::Preview
+  # Example
+  # ---
+  # ```html
+  # <body data-controller="lightbox">
+  #   <%= yield %>
+  # </body>
+  # ```
   def default
   end
 end
