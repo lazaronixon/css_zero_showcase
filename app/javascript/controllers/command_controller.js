7173c687065ead9583eb1187f41f7e7b4e1af10b
Nixon <lazaronixon@hotmail.com>
2024-09-28 04:36:26 -0300

    more work command

--- app/javascript/controllers/command_controller.js
+++ app/javascript/controllers/command_controller.js
@@ -2,14 +2,10 @@ import { Controller } from "@hotwired/stimulus"
 import { debounce } from "helpers/timing_helpers"
 
 export default class extends Controller {
-  static targets = [ "group", "option", "empty" ]
+  static targets = [ "list", "group", "option", "empty" ]
 
   initialize() {
     this.filter = debounce(this.filter.bind(this), 300)
-  }
-
-  connect() {
-    this.element.focus()
   }
 
   filter({ target }) {
@@ -23,20 +19,26 @@ export default class extends Controller {
   #selectMatches(value) {
     const selector = `[data-value*=${value.toLowerCase()}]`
 
-    this.groupTargets.forEach((element) => {
-      element.hidden = !element.querySelector(selector)
-    })
-
     this.optionTargets.forEach((element) => {
       element.hidden = !element.matches(selector)
     })
 
-    this.emptyTarget.hidden = this.element.querySelector(selector)
+    this.groupTargets.forEach((element) => {
+      element.hidden = !element.querySelector(selector)
+    })
+
+    this.emptyTarget.hidden = !!this.listTarget.querySelector(selector)
   }
 
   #reset() {
-    this.groupTargets.forEach(element => element.hidden = false)
-    this.optionTargets.forEach(element => element.hidden = false)
+    this.optionTargets.forEach((element) => {
+      element.hidden = false
+    })
+
+    this.groupTargets.forEach((element) => {
+      element.hidden = false
+    })
+
     this.emptyTarget.hidden = true
   }
 }
--- test/components/previews/command_preview.rb
+++ test/components/previews/command_preview.rb
@@ -1,3 +1,4 @@
+# @js app/javascript/controllers/command_controller.js
 class CommandPreview < Lookbook::Preview
   def default
   end
