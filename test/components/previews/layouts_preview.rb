448cef48f3c003ff0412cb173fe9756ee398635b
Nixon <lazaronixon@hotmail.com>
2024-10-01 02:20:44 -0300

    new layouts

--- test/components/previews/layouts_preview.rb
+++ test/components/previews/layouts_preview.rb
@@ -1,17 +1,12 @@
+# @display padding 0px
 # @css app/assets/stylesheets/layouts.css
 class LayoutsPreview < ViewComponent::Preview
+  def header_layout
+  end
+
   # Partials
   # ---
   # - [Mobile menu](https://github.com/lazaronixon/css_zero_showcase/blob/main/app/views/lookbook/preview/_mobile_menu.html.erb)
   def sidebar_layout
-  end
-
-  def pancake_stack
-  end
-
-  def super_centered
-  end
-
-  def container
   end
 end
