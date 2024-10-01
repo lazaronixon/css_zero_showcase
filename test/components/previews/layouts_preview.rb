8656a8cfce7b3c93f01e207955cbf780040a2706
Nixon <lazaronixon@hotmail.com>
2024-10-01 05:18:04 -0300

    centered layout

--- test/components/previews/layouts_preview.rb
+++ test/components/previews/layouts_preview.rb
@@ -9,4 +9,7 @@ class LayoutsPreview < ViewComponent::Preview
   # - [Mobile menu](https://github.com/lazaronixon/css_zero_showcase/blob/main/app/views/lookbook/preview/_mobile_menu.html.erb)
   def sidebar_layout
   end
+
+  def centered_layout
+  end
 end
