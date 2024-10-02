af38778c4a54bc6373a1f64811502a051e3b354d
Nixon <lazaronixon@hotmail.com>
2024-10-02 00:43:22 -0300

    More sheet examples

--- test/components/previews/sheet_preview.rb
+++ test/components/previews/sheet_preview.rb
@@ -1,6 +1,15 @@
 # @css app/assets/stylesheets/sheet.css
 # @js app/javascript/controllers/dialog_controller.js
 class SheetPreview < Lookbook::Preview
-  def default
+  # Partials
+  # ---
+  # - [profile_form](https://github.com/lazaronixon/css_zero_showcase/blob/main/app/views/lookbook/preview/_profile_form.html.erb)
+  def left
+  end
+
+  # Partials
+  # ---
+  # - [profile_form](https://github.com/lazaronixon/css_zero_showcase/blob/main/app/views/lookbook/preview/_profile_form.html.erb)
+  def right
   end
 end
