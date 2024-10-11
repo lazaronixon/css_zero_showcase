822145f03f224b264ff30ea3d439418babdd26e3
Nixon <lazaronixon@hotmail.com>
2024-10-10 22:05:10 -0300

    popover update

--- test/components/previews/popover_preview.rb
+++ test/components/previews/popover_preview.rb
@@ -1,4 +1,6 @@
 # @display padding 0px
+# @css app/assets/stylesheets/popover.css
+# @js app/javascript/controllers/popover_controller.js
 class PopoverPreview < Lookbook::Preview
   def default
   end
