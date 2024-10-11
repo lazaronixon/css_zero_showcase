fa4d04602321b047590ccc5a435b10323df00e12
Nixon <lazaronixon@hotmail.com>
2024-10-11 03:30:34 -0300

    add dropdown

--- test/components/previews/popover_preview.rb
+++ test/components/previews/popover_preview.rb
@@ -19,4 +19,7 @@ class PopoverPreview < Lookbook::Preview
   # - [dimensions_form](https://github.com/lazaronixon/css_zero_showcase/blob/main/app/views/lookbook/preview/_dimensions_form.html.erb)
   def position_custom
   end
+
+  def dropdown    
+  end
 end
