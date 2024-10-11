028598a48012dc16849860bb81c4d9a4654c6400
Nixon <lazaronixon@hotmail.com>
2024-10-11 01:07:16 -0300

    more update work

--- test/components/previews/popover_preview.rb
+++ test/components/previews/popover_preview.rb
@@ -2,6 +2,12 @@
 # @css app/assets/stylesheets/popover.css
 # @js app/javascript/controllers/popover_controller.js
 class PopoverPreview < Lookbook::Preview
-  def default
+  def position_start
+  end
+
+  def position_end
+  end
+
+  def position_custom
   end
 end
