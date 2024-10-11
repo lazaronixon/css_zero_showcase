b15584c9699b90395e222382b2537df0f7f0c7a2
Nixon <lazaronixon@hotmail.com>
2024-10-11 02:49:59 -0300

    notes popover

--- test/components/previews/popover_preview.rb
+++ test/components/previews/popover_preview.rb
@@ -2,12 +2,21 @@
 # @css app/assets/stylesheets/popover.css
 # @js app/javascript/controllers/popover_controller.js
 class PopoverPreview < Lookbook::Preview
+  # Partials
+  # ---
+  # - [dimensions_form](https://github.com/lazaronixon/css_zero_showcase/blob/main/app/views/lookbook/preview/_dimensions_form.html.erb)
   def position_start
   end
 
+  # Partials
+  # ---
+  # - [dimensions_form](https://github.com/lazaronixon/css_zero_showcase/blob/main/app/views/lookbook/preview/_dimensions_form.html.erb)
   def position_end
   end
 
+  # Partials
+  # ---
+  # - [dimensions_form](https://github.com/lazaronixon/css_zero_showcase/blob/main/app/views/lookbook/preview/_dimensions_form.html.erb)
   def position_custom
   end
 end
