6fdecaa2ff68e99674abc38cef6a20a1efd3da92
Nixon <lazaronixon@hotmail.com>
2024-10-14 02:49:19 -0300

    other dropdowns

--- test/components/previews/dropdown_preview.rb
+++ test/components/previews/dropdown_preview.rb
@@ -6,6 +6,20 @@ class DropdownPreview < Lookbook::Preview
   # - [popover_controller](https://github.com/lazaronixon/css_zero_showcase/blob/main/app/javascript/controllers/popover_controller.js)
   # - [menu_css](https://github.com/lazaronixon/css_zero_showcase/blob/main/app/assets/stylesheets/menu.css)
   # - [menu_controller](https://github.com/lazaronixon/css_zero_showcase/blob/main/app/javascript/controllers/menu_controller.js)
-  def default
+  def menu
+  end
+
+  # Links
+  # ---
+  # - [popover_css](https://github.com/lazaronixon/css_zero_showcase/blob/main/app/assets/stylesheets/popover.css)
+  # - [popover_controller](https://github.com/lazaronixon/css_zero_showcase/blob/main/app/javascript/controllers/popover_controller.js)
+  def checkbox
+  end
+
+  # Links
+  # ---
+  # - [popover_css](https://github.com/lazaronixon/css_zero_showcase/blob/main/app/assets/stylesheets/popover.css)
+  # - [popover_controller](https://github.com/lazaronixon/css_zero_showcase/blob/main/app/javascript/controllers/popover_controller.js)
+  def radio
   end
 end
