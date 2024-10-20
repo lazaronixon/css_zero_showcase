ea1c803d211f01e575dfbdb6942a2a595a0e7bfc
Nixon <lazaronixon@hotmail.com>
2024-10-20 03:09:56 -0300

    trix

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,6 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 import "initializers"
+
+import "trix"
+import "@rails/actiontext"
---
+++ test/components/previews/trix_preview.rb
@@ -0,0 +1,6 @@
+# @display max_width 700px
+# @css app/assets/stylesheets/ztrix.css
+class TrixPreview < Lookbook::Preview
+  def default
+  end
+end
