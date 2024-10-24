4f468de372ae6acf9b5ac374383c4d4629e0fde7
Nixon <lazaronixon@hotmail.com>
2024-10-23 21:03:49 -0300

    cosmetic combobox controller

--- app/javascript/controllers/combobox_controller.js
+++ app/javascript/controllers/combobox_controller.js
@@ -3,16 +3,15 @@ import TomSelect from "tom-select";
 
 export default class extends Controller {
   static values = {
-    variant: { type: String, default: "default" },
     createText: { type: String, default: "Add" },
     noResultsText: { type: String, default: "No results found" }
   }
 
   connect() {
-    if (this.variantValue === "tags") {
-      this.tomSelect = new TomSelect(this.element, this.#tagsOptions)
+    if (this.element.nodeName === "INPUT") {
+      this.tomSelect = new TomSelect(this.element, this.#inputOptions)
     } else {
-      this.tomSelect = new TomSelect(this.element, this.#defaultOptions)
+      this.tomSelect = new TomSelect(this.element, this.#selectOptions)
     }
   }
 
@@ -20,18 +19,22 @@ export default class extends Controller {
     this.tomSelect.destroy()
   }
 
-  get #tagsOptions() {
+  get #inputOptions() {
     return { render: this.#renderOptions, persist: false, createOnBlur: true, create: true }
   }
 
-  get #defaultOptions() {
+  get #selectOptions() {
     return { render: this.#renderOptions }
   }
 
   get #renderOptions() {
     return {
-      option_create: (data, escape) => `<div class="create">${this.createTextValue} <strong>${escape(data.input)}</strong>&hellip;</div>`,
-      no_results: () => `<div class="no-results">${this.noResultsTextValue}</div>`
+      option_create: (data, escape) => {
+        return `<div class="create">${this.createTextValue} <b>${escape(data.input)}</b>...</div>`
+      },
+      no_results: () =>  {
+        return `<div class="no-results">${this.noResultsTextValue}</div>`
+      }
     }
   }
 }
--- test/components/previews/combobox_preview.rb
+++ test/components/previews/combobox_preview.rb
@@ -2,7 +2,6 @@
 # @css app/assets/stylesheets/zcombobox.css
 # @js app/javascript/controllers/combobox_controller.js
 class ComboboxPreview < Lookbook::Preview
-  # @!group Default
   # Dependencies
   # ---
   # This component depends on [Tom Select](https://github.com/orchidjs/tom-select).
@@ -12,6 +11,13 @@ class ComboboxPreview < Lookbook::Preview
   def input
   end
 
+  # @!group Select
+  # Dependencies
+  # ---
+  # This component depends on [Tom Select](https://github.com/orchidjs/tom-select).
+  # You'll need to install it first before using this components.
+  #
+  # You can do this using the command `bin/importmaps pin tom-select`
   def single
   end
 
@@ -21,13 +27,25 @@ class ComboboxPreview < Lookbook::Preview
   def disabled_options
   end
 
-  def grouped_options    
+  def grouped_options
   end
+  # @!endgroup
 
+  # Dependencies
+  # ---
+  # This component depends on [Tom Select](https://github.com/orchidjs/tom-select).
+  # You'll need to install it first before using this components.
+  #
+  # You can do this using the command `bin/importmaps pin tom-select`
   def multiple
   end
 
+  # Dependencies
+  # ---
+  # This component depends on [Tom Select](https://github.com/orchidjs/tom-select).
+  # You'll need to install it first before using this components.
+  #
+  # You can do this using the command `bin/importmaps pin tom-select`
   def internationalization
   end
-  # @!endgroup
 end
