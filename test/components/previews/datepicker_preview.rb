daa78f7d768dabe0c5196e87a8d3a2fb2bd2be0c
Nixon <lazaronixon@hotmail.com>
2024-10-28 02:53:19 -0300

    Add date picker

---
+++ test/components/previews/datepicker_preview.rb
@@ -0,0 +1,27 @@
+# @display max_width 400px
+# @css app/assets/stylesheets/zdatepicker.css
+# @js app/javascript/controllers/datepicker_controller.js
+class DatepickerPreview < Lookbook::Preview
+  # @!group Default
+  # Dependencies
+  # ---
+  # - `bin/importmaps pin flatpickr`
+  def default
+  end
+
+  def datetime
+  end
+
+  def disabled_dates
+  end
+
+  def multiple
+  end
+
+  def range
+  end
+
+  def time
+  end
+  # @!endgroup
+end
