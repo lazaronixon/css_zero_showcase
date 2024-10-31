f6b9e82cf30315beb8e69e760d34034bfe4d88aa
Nixon <lazaronixon@hotmail.com>
2024-10-31 03:38:52 -0300

    Implement charts

---
+++ test/components/previews/chart_preview.rb
@@ -0,0 +1,12 @@
+# @display max_width 800px
+# @js app/javascript/controllers/chart_controller.js
+class ChartPreview < Lookbook::Preview
+  def bar_chart
+  end
+
+  def line_chart
+  end
+
+  def pie_chart
+  end
+end
