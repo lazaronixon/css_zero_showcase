8f6eec71ef55822f5661d00572646090fcfabe9b
Nixon <lazaronixon@hotmail.com>
2024-11-07 23:58:33 -0300

    update rails 8

--- config/application.rb
+++ config/application.rb
@@ -9,12 +9,12 @@ Bundler.require(*Rails.groups)
 module CssZeroShowcase
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.1
+    config.load_defaults 8.0
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w(assets tasks))
+    config.autoload_lib(ignore: %w[assets tasks])
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -26,7 +26,7 @@ module CssZeroShowcase
 
     config.lookbook.ui_theme = "zinc"
 
-    config.lookbook.preview_display_options = { theme: ["light", "dark"] }    
+    config.lookbook.preview_display_options = { theme: ["light", "dark"] }
 
     config.lookbook.project_logo = false
     config.lookbook.ui_favicon   = false
