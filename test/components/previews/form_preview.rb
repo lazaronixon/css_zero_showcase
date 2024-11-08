e48c8423d153f782a49beeaba4c8745bc4c0d9cb
Nixon <lazaronixon@hotmail.com>
2024-11-08 13:41:16 -0300

    More form examples

--- test/components/previews/form_preview.rb
+++ test/components/previews/form_preview.rb
@@ -1,5 +1,9 @@
+# @display max_width 400px
 # @js app/javascript/controllers/form_controller.js
 class FormPreview < Lookbook::Preview
-  def default
+  def submit_on_change
+  end
+
+  def submit_on_cmd_enter
   end
 end
