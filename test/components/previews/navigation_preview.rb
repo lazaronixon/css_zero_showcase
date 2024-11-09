e4914a8dbf09b5269fce494153469ff64fb844ad
Nixon <lazaronixon@hotmail.com>
2024-11-09 01:17:36 -0300

    add navigation controller

---
+++ test/components/previews/navigation_preview.rb
@@ -0,0 +1,13 @@
+# @js app/javascript/controllers/navigation_controller.js
+class NavigationPreview < Lookbook::Preview
+  # @!group Default
+  def back
+  end
+
+  def forward
+  end
+
+  def reload
+  end
+  # @!endgroup
+end
