3534e49f8d47e552be034f998b1073222c8f99af
Nixon <lazaronixon@hotmail.com>
2024-11-25 00:28:32 -0300

    autonimate css

--- test/components/previews/autoanimate_preview.rb
+++ test/components/previews/autoanimate_preview.rb
@@ -1,13 +1,15 @@
-# @js app/javascript/controllers/autoanimate_controller.js
+# @css app/assets/stylesheets/autoanimate.css
 class AutoanimatePreview < Lookbook::Preview
   # Implementation
   # ---
-  # - [fruits_controller.rb](https://github.com/lazaronixon/css_zero_showcase/blob/main/app/controllers/fruits_controller.rb)
   # - [index.html.erb](https://github.com/lazaronixon/css_zero_showcase/blob/main/app/views/fruits/index.html.erb)
-  #
-  # Dependencies
-  # ---
-  # - [Auto animate](https://github.com/formkit/auto-animate)
+  # - [fruits_controller.rb](https://github.com/lazaronixon/css_zero_showcase/blob/main/app/controllers/fruits_controller.rb)
   def default
+  end
+
+  # These animations are available as CSS variables at
+  # [animation.css](https://github.com/lazaronixon/css-zero/blob/master/app/assets/stylesheets/animations.css).
+  # The animation stimulus controller is for demonstration purposes only.
+  def variables
   end
 end
