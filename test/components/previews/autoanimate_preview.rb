8780e78a2f2c4cd7f3e5f37477cf2237414db37b
Nixon <lazaronixon@hotmail.com>
2025-01-10 01:43:04 -0300

    Remove animation variables example

--- test/components/previews/autoanimate_preview.rb
+++ test/components/previews/autoanimate_preview.rb
@@ -6,10 +6,4 @@ class AutoanimatePreview < Lookbook::Preview
   # - [fruits_controller.rb](https://github.com/lazaronixon/css_zero_showcase/blob/main/app/controllers/fruits_controller.rb)
   def default
   end
-
-  # These animations are available as CSS variables at
-  # [animation.css](https://github.com/lazaronixon/css-zero/blob/master/app/assets/stylesheets/animations.css).
-  # The animation stimulus controller is for demonstration purposes only.
-  def variables
-  end
 end
