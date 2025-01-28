7757e5e183416d7d06c76449b800e3020efa3ab2
Nixon <lazaronixon@hotmail.com>
2025-01-28 01:46:14 -0300

    Add header menu partials

--- test/components/previews/layouts_preview.rb
+++ test/components/previews/layouts_preview.rb
@@ -1,18 +1,31 @@
 # @display padding 0px
 # @css app/assets/stylesheets/layouts.css
 class LayoutsPreview < ViewComponent::Preview
+  # Partials
+  # ---
+  # - [\_mobile_menu.html.erb](https://github.com/lazaronixon/css_zero_showcase/blob/main/app/views/lookbook/preview/_mobile_menu.html.erb)
+  # - [\_header_menu.html.erb](https://github.com/lazaronixon/css_zero_showcase/blob/main/app/views/lookbook/preview/_header_menu.html.erb)
   def header_layout_1
   end
 
+  # Partials
+  # ---
+  # - [\_mobile_menu.html.erb](https://github.com/lazaronixon/css_zero_showcase/blob/main/app/views/lookbook/preview/_mobile_menu.html.erb)
+  # - [\_header_menu.html.erb](https://github.com/lazaronixon/css_zero_showcase/blob/main/app/views/lookbook/preview/_header_menu.html.erb)
   def header_layout_2
   end
 
+  # Partials
+  # ---
+  # - [\_mobile_menu.html.erb](https://github.com/lazaronixon/css_zero_showcase/blob/main/app/views/lookbook/preview/_mobile_menu.html.erb)
+  # - [\_header_menu.html.erb](https://github.com/lazaronixon/css_zero_showcase/blob/main/app/views/lookbook/preview/_header_menu.html.erb)
   def sticky_footer
   end
 
   # Partials
   # ---
   # - [\_mobile_menu.html.erb](https://github.com/lazaronixon/css_zero_showcase/blob/main/app/views/lookbook/preview/_mobile_menu.html.erb)
+  # - [\_header_menu.html.erb](https://github.com/lazaronixon/css_zero_showcase/blob/main/app/views/lookbook/preview/_header_menu.html.erb)
   def sidebar_layout_1
   end
 
