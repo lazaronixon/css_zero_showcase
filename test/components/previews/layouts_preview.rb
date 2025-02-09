b81b55d04c112bcb705044f9db728a90c800e039
Nixon <lazaronixon@hotmail.com>
2025-02-08 23:36:27 -0300

    Add sidebar menu

--- test/components/previews/layouts_preview.rb
+++ test/components/previews/layouts_preview.rb
@@ -26,12 +26,14 @@ class LayoutsPreview < ViewComponent::Preview
   # ---
   # - [\_mobile_menu.html.erb](https://github.com/lazaronixon/css_zero_showcase/blob/main/app/views/lookbook/preview/_mobile_menu.html.erb)
   # - [\_header_menu.html.erb](https://github.com/lazaronixon/css_zero_showcase/blob/main/app/views/lookbook/preview/_header_menu.html.erb)
+  # - [\_sidebar_menu.html.erb](https://github.com/lazaronixon/css_zero_showcase/blob/main/app/views/lookbook/preview/_sidebar_menu.html.erb)
   def sidebar_layout_1
   end
 
   # Partials
   # ---
   # - [\_mobile_menu.html.erb](https://github.com/lazaronixon/css_zero_showcase/blob/main/app/views/lookbook/preview/_mobile_menu.html.erb)
+  # - [\_sidebar_menu.html.erb](https://github.com/lazaronixon/css_zero_showcase/blob/main/app/views/lookbook/preview/_sidebar_menu.html.erb)
   def sidebar_layout_2
   end
 
