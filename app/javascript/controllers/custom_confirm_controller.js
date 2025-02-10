30187c7968f70e98682d80dd1eecf6eb16b47900
Nixon <lazaronixon@hotmail.com>
2025-02-09 23:17:52 -0300

    adjust import custom_confirm

--- app/javascript/controllers/custom_confirm_controller.js
+++ app/javascript/controllers/custom_confirm_controller.js
@@ -1,5 +1,5 @@
 import { Controller } from "@hotwired/stimulus"
-import Swal from "https://esm.sh/sweetalert2@11.16.0/dist/sweetalert2.js"
+import Swal from "https://esm.sh/sweetalert2@11.16.0/dist/sweetalert2.js?standalone"
 
 export default class extends Controller {
   connect() {
