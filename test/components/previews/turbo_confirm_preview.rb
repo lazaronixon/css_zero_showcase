b517a7908682a635ded18fdf2c83102c18b8a3e9
Nixon <lazaronixon@hotmail.com>
2025-02-10 12:54:15 -0300

    rename custom-confirm to turbo-confirm

---
+++ test/components/previews/turbo_confirm_preview.rb
@@ -0,0 +1,16 @@
+# @js app/javascript/controllers/turbo_confirm_controller.js
+# @css app/assets/stylesheets/turbo_confirm.css
+class TurboConfirmPreview < Lookbook::Preview
+  # @!group Default
+  # Initialization
+  # ---
+  # ```html
+  # <body data-controller="turbo-confirm">...</body>
+  # ```
+  def button
+  end
+
+  def link
+  end
+  # @!endgroup
+end
