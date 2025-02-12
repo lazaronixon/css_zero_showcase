0aa8947efdcf9dda9ea325980fc652d8c3f2f982
Nixon <lazaronixon@hotmail.com>
2025-02-11 22:52:19 -0300

    refactor paginable controller

--- app/controllers/paginables_controller.rb
+++ app/controllers/paginables_controller.rb
@@ -1,6 +1,10 @@
 class PaginablesController < ApplicationController
   def index
     set_page_and_extract_portion_from Movie.order(:name), per_page: 10
+    slow_down_next_pages
+  end
+
+  def slow_down_next_pages
     sleep 2.seconds unless @page.first?
   end
 end
