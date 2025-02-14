952235eeb2645e0e7c2f50a09f42f4b64bfb3377
Nixon <lazaronixon@hotmail.com>
2025-02-14 03:08:23 -0300

    datatable filtering

--- test/components/previews/table_preview.rb
+++ test/components/previews/table_preview.rb
@@ -4,6 +4,15 @@ class TablePreview < Lookbook::Preview
   def default
   end
 
+  # Implementation
+  # ---
+  # - [tasks_controller.rb](https://github.com/lazaronixon/css_zero_showcase/blob/main/app/controllers/tasks_controller.rb)
+  # - [index.html.erb](https://github.com/lazaronixon/css_zero_showcase/blob/main/app/views/tasks/index.html.erb)
+  #
+  # Dependencies
+  # ---
+  # - [Pagy](https://github.com/ddnexus/pagy)
+  # - [Ransack](https://github.com/activerecord-hackery/ransack)
   def data_table
   end
 end
