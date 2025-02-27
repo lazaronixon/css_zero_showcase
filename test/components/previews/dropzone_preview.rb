c91997ba854debc86b6dc79375c51c7909975a09
Nixon <lazaronixon@hotmail.com>
2025-02-27 02:18:25 -0300

    notes

--- test/components/previews/dropzone_preview.rb
+++ test/components/previews/dropzone_preview.rb
@@ -2,9 +2,15 @@
 # @css app/assets/stylesheets/dropzone.css
 # @js app/javascript/controllers/dropzone_controller.js
 class DropzonePreview < Lookbook::Preview
+  # Dependencies
+  # ---
+  # - [Dropzone](https://github.com/dropzone/dropzone)
   def default
   end
 
+  # Dependencies
+  # ---
+  # - [Dropzone](https://github.com/dropzone/dropzone)
   def validation
   end
 end
