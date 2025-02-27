717057e0b608f4404f9d0604eef566fc46873411
Nixon <lazaronixon@hotmail.com>
2025-02-27 01:32:14 -0300

    Dropzone component

---
+++ test/components/previews/dropzone_preview.rb
@@ -0,0 +1,10 @@
+# @display max_width 650px
+# @css app/assets/stylesheets/dropzone.css
+# @js app/javascript/controllers/dropzone_controller.js
+class DropzonePreview < Lookbook::Preview
+  def default
+  end
+
+  def validation
+  end
+end
