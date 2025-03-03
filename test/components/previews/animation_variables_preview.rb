8f250e62cbc7f6aa05cf57acf2afc2f901f1ae2c
Nixon <lazaronixon@hotmail.com>
2025-03-03 02:24:47 -0300

    rename to animation variables

--- test/components/previews/animation_variables_preview.rb
+++ test/components/previews/animation_variables_preview.rb
@@ -1,4 +1,4 @@
-class AnimationPreview < Lookbook::Preview
+class AnimationVariablesPreview < Lookbook::Preview
   # This stimulus controller is for demonstration purposes only.
   #
   # [Animation Variables](https://github.com/lazaronixon/css-zero/blob/master/app/assets/stylesheets/css-zero/animations.css)
