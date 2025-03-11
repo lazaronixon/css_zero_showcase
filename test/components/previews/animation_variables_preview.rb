856175e1f90b273b28ab3602772664dba638ecc2
Nixon <lazaronixon@hotmail.com>
2025-03-11 12:08:20 -0300

    Remove gradient and animation variables

--- test/components/previews/animation_variables_preview.rb
+++
@@ -1,7 +0,0 @@
-class AnimationVariablesPreview < Lookbook::Preview
-  # This stimulus controller is for demonstration purposes only.
-  #
-  # [Animation Variables](https://github.com/lazaronixon/css-zero/blob/master/app/assets/stylesheets/css-zero/animations.css)
-  def default
-  end
-end
