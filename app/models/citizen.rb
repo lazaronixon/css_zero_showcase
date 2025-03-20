00d5bcfb65aa8b75336a39cea10034f0de5a4656
Nixon <lazaronixon@hotmail.com>
2025-03-19 23:53:21 -0300

    Update citizen.rb

--- app/models/citizen.rb
+++ app/models/citizen.rb
@@ -1,9 +1,6 @@
 class Citizen < ApplicationRecord
   belongs_to :city
 
-  delegate :state,    to: :city,  allow_nil: true
-  delegate :state_id, to: :city,  allow_nil: true
-
-  delegate :country,    to: :state, allow_nil: true
-  delegate :country_id, to: :state, allow_nil: true
+  delegate :state, :state_id, to: :city, allow_nil: true
+  delegate :country, :country_id, to: :state, allow_nil: true
 end
