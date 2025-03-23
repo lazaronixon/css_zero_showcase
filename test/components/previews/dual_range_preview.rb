877f02f0acc3c4aee777a7b29fc8efdcb1294b18
Nixon <lazaronixon@hotmail.com>
2025-03-23 00:53:01 -0300

    Add dual range slider component

---
+++ test/components/previews/dual_range_preview.rb
@@ -0,0 +1,7 @@
+  # @display max_width 400px
+  # @js app/javascript/controllers/dual_range_controller.js
+  # @css app/assets/stylesheets/dual_range.css
+  class DualRangePreview < Lookbook::Preview
+    def default
+    end
+  end
