eb9c57a1bac2c8325d60fcc9e771639dd9fe8543
Nixon <lazaronixon@hotmail.com>
2025-05-20 20:57:49 -0300

    Update dependencies

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,5 +5,5 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "trix", to: "trix.esm.min.js" # @2.1.12
+pin "trix", to: "trix.esm.min.js" # @2.1.15
 pin "@rails/actiontext", to: "actiontext.esm.js"
