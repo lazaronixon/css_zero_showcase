25d3ac767a836bea10e48205ba850a18be307846
Nixon <lazaronixon@hotmail.com>
2025-06-21 17:31:59 -0300

    Use icons component everywhere

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,9 +1,9 @@
 module TasksHelper
   def status_icons
-    { "in_progress" => "timer.svg", "done" => "circle-check-big.svg", "backlog" => "circle-help.svg", "todo" => "circle.svg", "canceled" => "circle-off.svg" }
+    { "in_progress" => "icon--clock", "done" => "icon--check-circle", "backlog" => "icon--question-mark-circle", "todo" => "icon--information-circle", "canceled" => "icon--x-circle" }
   end
 
   def priority_icons
-    { "high" => "arrow-up.svg", "low" => "arrow-down.svg", "medium" => "arrow-right.svg" }
+    { "high" => "icon--arrow-up", "low" => "icon--arrow-down", "medium" => "icon--arrow-right" }
   end
 end
---
+++ test/components/previews/icons_preview.rb
@@ -0,0 +1,17 @@
+# @display max_width 450px
+# @css app/assets/stylesheets/icons.css
+class IconsPreview < Lookbook::Preview
+  # @!group Icon
+  def default
+  end
+
+  def color
+  end
+
+  def size
+  end
+
+  def loading
+  end
+  # @!endgroup
+end
