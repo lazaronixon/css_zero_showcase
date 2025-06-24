f6a58a60ccf45392432a168bd14b73c68226da6c
Nixon <lazaronixon@hotmail.com>
2025-06-24 15:11:23 -0300

    Move color-scheme to html tag

--- test/components/previews/color_scheme_preview.rb
+++ test/components/previews/color_scheme_preview.rb
@@ -1,6 +1,11 @@
 # @css app/assets/stylesheets/color_scheme.css
 # @js app/javascript/controllers/color_scheme_controller.js
 class ColorSchemePreview < Lookbook::Preview
+  # Initialization
+  # ---
+  # ```html
+  # <html data-controller="color-scheme">...</html>
+  # ```
   def default
   end
 end
