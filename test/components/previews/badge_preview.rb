9e4f40ad8c5df02cbbfb82d98ac7cda9732345fc
Nixon <lazaronixon@hotmail.com>
2025-06-25 02:41:56 -0300

    Improve badges

--- test/components/previews/badge_preview.rb
+++ test/components/previews/badge_preview.rb
@@ -1,19 +1,5 @@
 # @css app/assets/stylesheets/badge.css
 class BadgePreview < Lookbook::Preview
-  # @!group Default
   def default
   end
-
-  def primary
-  end
-
-  def secondary
-  end
-
-  def positive
-  end
-
-  def negative
-  end
-  # @!endgroup
 end
