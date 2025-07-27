c06a5b97bd221dbeee1503cad34c598921cd0883
Nixon <lazaronixon@hotmail.com>
2025-07-27 19:41:50 -0300

    Add resizable split panel component with previews and styles

--- test/components/previews/resizable_preview.rb
+++ test/components/previews/resizable_preview.rb
@@ -1,6 +1,10 @@
 # @display max_width 450px
 # @css app/assets/stylesheets/resizable.css
+# @js app/javascript/controllers/resizable_controller.js
 class ResizablePreview < Lookbook::Preview
+  def default
+  end
+
   def vertical
   end
 
