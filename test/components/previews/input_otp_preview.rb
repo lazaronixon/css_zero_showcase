afe82087c338b1551b1007819eead1cfd4fab33f
Nixon <lazaronixon@hotmail.com>
2025-07-29 13:58:57 -0300

    Refactor input_otp_controller to use target for input field

--- test/components/previews/input_otp_preview.rb
+++ test/components/previews/input_otp_preview.rb
@@ -1,6 +1,9 @@
 # @display max_width 400px
 # @js app/javascript/controllers/input_otp_controller.js
 class InputOtpPreview < Lookbook::Preview
+
+  # Links
+  # - [WebOTP API](https://developer.mozilla.org/en-US/docs/Web/API/WebOTP_API)
   def default
   end
 end
