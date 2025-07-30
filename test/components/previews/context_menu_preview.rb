fe3636af1209292ca1f8b38bf249f1ac720cbdfd
Nixon <lazaronixon@hotmail.com>
2025-07-30 12:18:45 -0300

    Extract context menu preview to its own file

---
+++ test/components/previews/context_menu_preview.rb
@@ -0,0 +1,5 @@
+# @js app/javascript/controllers/context_menu_controller.js
+class ContextMenuPreview < Lookbook::Preview
+  def default
+  end
+end
--- test/components/previews/dropdown_preview.rb
+++ test/components/previews/dropdown_preview.rb
@@ -1,60 +1,15 @@
+# @css app/assets/stylesheets/menu.css
+# @js app/javascript/controllers/menu_controller.js
 class DropdownPreview < Lookbook::Preview
-  # Links
-  # ---
-  # - [popover.css](https://github.com/lazaronixon/css_zero_showcase/blob/main/app/assets/stylesheets/popover.css)
-  # - [popover_controller.js](https://github.com/lazaronixon/css_zero_showcase/blob/main/app/javascript/controllers/popover_controller.js)
-  # - [menu.css](https://github.com/lazaronixon/css_zero_showcase/blob/main/app/assets/stylesheets/menu.css)
-  # - [menu_controller.js](https://github.com/lazaronixon/css_zero_showcase/blob/main/app/javascript/controllers/menu_controller.js)
-  #
-  # Dependencies
-  # ---
-  # - [Floating UI](https://floating-ui.com)
   def menu_simple
   end
 
-  # Links
-  # ---
-  # - [popover.css](https://github.com/lazaronixon/css_zero_showcase/blob/main/app/assets/stylesheets/popover.css)
-  # - [popover_controller.js](https://github.com/lazaronixon/css_zero_showcase/blob/main/app/javascript/controllers/popover_controller.js)
-  # - [menu.css](https://github.com/lazaronixon/css_zero_showcase/blob/main/app/assets/stylesheets/menu.css)
-  # - [menu_controller.js](https://github.com/lazaronixon/css_zero_showcase/blob/main/app/javascript/controllers/menu_controller.js)
-  # - [_menu_items.html.erb](https://github.com/lazaronixon/css_zero_showcase/blob/main/app/views/lookbook/preview/_menu_items.html.erb)
-  #
-  # Dependencies
-  # ---
-  # - [Floating UI](https://floating-ui.com)
   def menu_complete
   end
 
-  # Links
-  # ---
-  # - [popover.css](https://github.com/lazaronixon/css_zero_showcase/blob/main/app/assets/stylesheets/popover.css)
-  # - [context_menu_controller.js](https://github.com/lazaronixon/css_zero_showcase/blob/main/app/javascript/controllers/context_menu_controller.js)
-  # - [menu.css](https://github.com/lazaronixon/css_zero_showcase/blob/main/app/assets/stylesheets/menu.css)
-  # - [_menu_items.html.erb](https://github.com/lazaronixon/css_zero_showcase/blob/main/app/views/lookbook/preview/_menu_items.html.erb)
-  #
-  def context_menu
-  end
-
-  # Links
-  # ---
-  # - [popover.css](https://github.com/lazaronixon/css_zero_showcase/blob/main/app/assets/stylesheets/popover.css)
-  # - [popover_controller.js](https://github.com/lazaronixon/css_zero_showcase/blob/main/app/javascript/controllers/popover_controller.js)
-  #
-  # Dependencies
-  # ---
-  # - [Floating UI](https://floating-ui.com)
   def checkbox
   end
 
-  # Links
-  # ---
-  # - [popover.css](https://github.com/lazaronixon/css_zero_showcase/blob/main/app/assets/stylesheets/popover.css)
-  # - [popover_controller.js](https://github.com/lazaronixon/css_zero_showcase/blob/main/app/javascript/controllers/popover_controller.js)
-  #
-  # Dependencies
-  # ---
-  # - [Floating UI](https://floating-ui.com)
   def radio
   end
 end
