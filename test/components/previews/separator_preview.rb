5770959e777a9ab11aef4e73d4760303b6f19b7e
Lazaro Nixon <lazaronixon@hotmail.com>
2025-11-07 04:14:06 -0300

    Add separator CSS and update separator preview examples

--- test/components/previews/separator_preview.rb
+++ test/components/previews/separator_preview.rb
@@ -1,19 +1,6 @@
 # @display max_width 450px
+# @css app/assets/stylesheets/separator.css
 class SeparatorPreview < Lookbook::Preview
-  # @!group Separator
   def default
   end
-
-  def inline_center
-  end
-
-  def inline_solid
-  end
-
-  def inline_dashed
-  end
-
-  def vertical
-  end
-  # @!endgroup
 end
