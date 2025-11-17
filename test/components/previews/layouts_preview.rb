cedf0eb38acc4d54d95d8546c5eae00741656465
Lazaro Nixon <lazaronixon@hotmail.com>
2025-11-17 02:28:07 -0300

    Update sidebar_layout_2 preview to use breadcrumb menu

--- test/components/previews/layouts_preview.rb
+++ test/components/previews/layouts_preview.rb
@@ -33,7 +33,7 @@ class LayoutsPreview < ViewComponent::Preview
 
   # Links
   # ---
-  # - [\_mobile_menu.html.erb](https://github.com/lazaronixon/css_zero_showcase/blob/main/app/views/lookbook/preview/_mobile_menu.html.erb)
+  # - [\_breadcrumb_menu.html.erb](https://github.com/lazaronixon/css_zero_showcase/blob/main/app/views/lookbook/preview/_mobile_menu.html.erb)
   # - [\_sidebar_menu.html.erb](https://github.com/lazaronixon/css_zero_showcase/blob/main/app/views/lookbook/preview/_sidebar_menu.html.erb)
   # - [sidebar_menu.css](https://github.com/lazaronixon/css_zero_showcase/blob/main/app/assets/stylesheets/sidebar_menu.css)
   def sidebar_layout_2
