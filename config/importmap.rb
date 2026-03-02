2fe45c25a8655b1373b9457b623a3b50f0225220
Lazaro Nixon <lazaronixon@hotmail.com>
2026-03-02 01:56:09 -0300

    update dependencies

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,5 +5,5 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "trix", to: "trix.esm.min.js" # @2.1.15
+pin "trix", to: "trix.esm.min.js" # @2.1.16
 pin "@rails/actiontext", to: "actiontext.esm.js"
--- test/components/previews/dropzone_preview.rb
+++ test/components/previews/dropzone_preview.rb
@@ -4,13 +4,13 @@
 class DropzonePreview < Lookbook::Preview
   # Dependencies
   # ---
-  # - [Dropzone](https://github.com/dropzone/dropzone)
+  # - [Dropzone](https://github.com/NicolasCARPi/dropzone)
   def default
   end
 
   # Dependencies
   # ---
-  # - [Dropzone](https://github.com/dropzone/dropzone)
+  # - [Dropzone](https://github.com/NicolasCARPi/dropzone)
   def validation
   end
 end
