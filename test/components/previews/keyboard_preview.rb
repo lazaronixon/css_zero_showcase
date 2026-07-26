14e3595d7e44115ea8e128559eafdeab329ab240
Lazaro Nixon <lazaronixon@hotmail.com>
2026-07-26 02:33:21 -0300

    keyboard component

---
+++ test/components/previews/keyboard_preview.rb
@@ -0,0 +1,14 @@
+# @css app/assets/stylesheets/keyboard.css
+class KeyboardPreview < Lookbook::Preview
+  def default
+  end
+
+  def button
+  end
+
+  def input
+  end
+
+  def tooltip
+  end
+end
