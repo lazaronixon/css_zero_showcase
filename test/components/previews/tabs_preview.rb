a06033188ee405de1c8125a50bd39b5991864f0d
Lazaro Nixon <lazaronixon@hotmail.com>
2026-07-30 03:27:40 -0300

    simplify tabs example

--- test/components/previews/tabs_preview.rb
+++ test/components/previews/tabs_preview.rb
@@ -1,10 +1,6 @@
 # @css app/assets/stylesheets/tabs.css
 # @js app/javascript/controllers/tabs_controller.js
 class TabsPreview < Lookbook::Preview
-  # Partials
-  # ---
-  # - [\_account_form.html.erb](https://github.com/lazaronixon/css_zero_showcase/blob/main/app/views/lookbook/preview/_account_form.html.erb)
-  # - [\_password_form.html.erb](https://github.com/lazaronixon/css_zero_showcase/blob/main/app/views/lookbook/preview/_password_form.html.erb)
   def complete
   end
 
