68f73bc3801df98806beb2d71483d2970256e454
Lazaro Nixon <lazaronixon@hotmail.com>
2026-08-09 00:44:10 -0300

    command

--- test/components/previews/command_preview.rb
+++ test/components/previews/command_preview.rb
@@ -1,23 +1,21 @@
 # @js app/javascript/controllers/command_controller.js
 # @css app/assets/stylesheets/command.css
 class CommandPreview < Lookbook::Preview
-  # Links
-  # ---
-  # - [\_command_options.html.erb](https://github.com/lazaronixon/css_zero_showcase/blob/main/app/views/lookbook/preview/_command_options.html.erb)
-  #
   # Dependencies
   # ---
   # - [combobox-nav](https://github.com/github/combobox-nav)
   def basic
   end
 
-  # Links
-  # ---
-  # - [\_command_options.html.erb](https://github.com/lazaronixon/css_zero_showcase/blob/main/app/views/lookbook/preview/_command_options.html.erb)
-  #
   # Dependencies
   # ---
   # - [combobox-nav](https://github.com/github/combobox-nav)
-  def dialog
+  def shortcuts
+  end
+
+  # Dependencies
+  # ---
+  # - [combobox-nav](https://github.com/github/combobox-nav)
+  def groups
   end
 end
