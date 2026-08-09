782e0236a95df5a36543551283fb856ef0d9e000
Lazaro Nixon <lazaronixon@hotmail.com>
2026-08-08 22:52:50 -0300

    badge

--- test/components/previews/badge_preview.rb
+++ test/components/previews/badge_preview.rb
@@ -1,5 +1,17 @@
 # @css app/assets/stylesheets/badge.css
 class BadgePreview < Lookbook::Preview
-  def basic
+  def variants
+  end
+
+  def colors
+  end
+
+  def with_icon
+  end
+
+  def with_spinner
+  end
+
+  def link
   end
 end
