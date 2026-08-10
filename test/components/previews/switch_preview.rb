68952408c690a0a39e728b5b42ab98afdbc476ee
Lazaro Nixon <lazaronixon@hotmail.com>
2026-08-09 21:28:43 -0300

    switch

--- test/components/previews/switch_preview.rb
+++ test/components/previews/switch_preview.rb
@@ -3,4 +3,18 @@
 class SwitchPreview < Lookbook::Preview
   def basic
   end
+
+  # @display max_width 384px
+  def description
+  end
+
+  # @display max_width 384px
+  def choice_card
+  end
+
+  def size
+  end
+
+  def disabled
+  end
 end
