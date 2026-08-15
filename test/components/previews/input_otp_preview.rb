ab612b144fb2e9af3ea4cb61a366f426ba34ee2e
Lazaro Nixon <lazaronixon@hotmail.com>
2026-08-15 14:24:36 -0300

    input otp

--- test/components/previews/input_otp_preview.rb
+++ test/components/previews/input_otp_preview.rb
@@ -1,6 +1,34 @@
+# @display max_width 344px
 # @css app/assets/stylesheets/input_otp.css
-# @js app/javascript/controllers/form_controller.js
+# @js app/javascript/controllers/input_otp_controller.js
 class InputOtpPreview < Lookbook::Preview
+  # Dependencies
+  # ---
+  # - [input-otp-zero](https://github.com/lazaronixon/input-otp-zero)
+  #
+  # Initialization
+  # ---
+  # ```html
+  # <body data-controller="input-otp">...</body>
+  # ```
   def basic
+  end
+
+  def pattern
+  end
+
+  def separator
+  end
+
+  def alphanumeric
+  end
+
+  def four_digits
+  end
+
+  def disabled
+  end
+
+  def form
   end
 end
