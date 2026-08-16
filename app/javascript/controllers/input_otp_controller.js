d33c4cf1f5f57bd11186d2926ddf9d4210ad82fc
Lazaro Nixon <lazaronixon@hotmail.com>
2026-08-16 02:28:54 -0300

    new version input-otp-zero

--- app/javascript/controllers/input_otp_controller.js
+++ app/javascript/controllers/input_otp_controller.js
@@ -1,5 +1,5 @@
 import { Controller } from "@hotwired/stimulus"
-import { defineElements } from "https://esm.sh/input-otp-zero@0.0.2?standalone"
+import { defineElements } from "https://esm.sh/input-otp-zero@0.0.3?standalone"
 
 export default class extends Controller {
   connect() {
