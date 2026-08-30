7b83a176e41fcad9f9b40062262c6ec546d5037a
Lazaro Nixon <lazaronixon@hotmail.com>
2026-08-30 13:15:58 -0300

    revert stimulus preload

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,7 +4,7 @@ pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
-pin_all_from "app/javascript/controllers", under: "controllers", preload: false
+pin_all_from "app/javascript/controllers", under: "controllers"
 pin "lexxy", to: "lexxy.js"
 pin "@rails/actiontext", to: "actiontext.esm.js"
 pin "input-otp-zero", to: "input-otp-zero.js"
